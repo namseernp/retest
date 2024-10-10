b0ca66d877c526a072184fb055902acbef9210e7
namseernp <namseer07@gmail.com>
2024-10-10 15:32:37 +0530

    My name changed

---
+++ namseer.c
@@ -0,0 +1,9 @@
+ublic class PrintWord {
+    public static void main(String[] args) {
+        // Define the word you want to print
+        String word = "Hello, World!";
+        
+        // Print the word to the console
+        System.out.println(hi world);
+    }
+}
